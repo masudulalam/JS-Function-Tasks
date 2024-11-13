9ec868d8977394698ab75c4cc31a455ad3f5e255
masudulalam <masuudulalam@gmail.com>
2024-11-14 03:42:14 +0600

    task1 is done

--- task1.js
+++ task1.js
@@ -6,3 +6,14 @@
  * 
  */
 
+
+// Solution:
+function multiplication (num1, num2, num3, num4) {
+    const result = num1 * num2 * num3 * num4;
+
+    return result
+}
+
+const output = multiplication(2, 3, 4, 5);
+
+console.log(output);
